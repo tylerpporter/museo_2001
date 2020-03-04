a7c58cfee1bbe84493dfabbfc64862d9a4a0d067
Tyler Porter <tylerpporter@gmail.com>
2020-03-04 09:13:49 -0700

    Create photograph class

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,1 +1,14 @@
+class Photograph
+  attr_reader :id,
+              :name,
+              :artist_id,
+              :year
 
+  def initialize(params)
+    @id = params[:id]
+    @name = params[:name]
+    @artist_id = params[:artist_id]
+    @year = params[:year]
+  end
+  
+end
---
+++ test/photograph_test.rb
@@ -0,0 +1,27 @@
+require_relative 'test_helper.rb'
+require './lib/photograph.rb'
+
+class PhotographTest < Minitest::Test
+
+  def setup
+    attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "4",
+      year: "1954"
+    }
+    @photograph = Photograph.new(attributes)
+  end
+
+  def test_it_exists
+    assert_instance_of Photograph, @photograph
+  end
+
+  def test_it_has_attributes
+    assert_equal "1", @photograph.id
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", @photograph.name
+    assert_equal "4", @photograph.artist_id
+    assert_equal "1954", @photograph.year 
+  end
+
+end
