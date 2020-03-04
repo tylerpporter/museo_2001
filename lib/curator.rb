e5645a8f4d621b93f5f74803876811834c8e4e5b
Tyler Porter <tylerpporter@gmail.com>
2020-03-04 09:32:34 -0700

    Create find_artist_by_id method

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,11 +1,22 @@
 class Curator
-  attr_reader :photographs
+  attr_reader :photographs,
+              :artists
 
   def initialize
     @photographs = []
+    @artists = []
   end
 
   def add_photograph(photo)
-    @photographs << photo 
+    @photographs << photo
   end
+
+  def add_artist(artist)
+    @artists << artist
+  end
+
+  def find_artist_by_id(id)
+    @artists.find {|artist| artist.id == id}
+  end
+
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -1,6 +1,7 @@
 require_relative 'test_helper.rb'
 require './lib/curator.rb'
 require './lib/photograph.rb'
+require './lib/artist.rb'
 
 class CuratorTest < Minitest::Test
 
@@ -18,17 +19,47 @@ class CuratorTest < Minitest::Test
          artist_id: "2",
          year: "1941"
     })
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+    })
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+    })
   end
 
   def test_it_exists
     assert_instance_of Curator, @curator
   end
 
-  def test_it_starts_with_no_photographs_but_can_add_them
+  def test_it_has_attributes
     assert_equal [], @curator.photographs
+    assert_equal [], @curator.artists
+  end
+
+  def test_it_can_add_photos
     @curator.add_photograph(@photo_1)
     @curator.add_photograph(@photo_2)
     assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+
+  def test_it_can_add_artists
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    assert_equal [@artist_1, @artist_2], @curator.artists
+  end
+
+  def test_it_can_find_artist_by_id
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    assert_equal @artist_1, @curator.find_artist_by_id("1")
   end
 
 end
